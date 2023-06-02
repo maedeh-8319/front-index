93a3313d5384beb336bcc4af992195a91789c40a
maedeh-8319 <maedehshobeiri83@gmail.com>
2023-06-02 13:11:16 +0330

    part #21

--- front/js/alpinejscontollers/userscontollores.js
+++ front/js/alpinejscontollers/userscontollores.js
@@ -1,17 +1,15 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('usersData', function(){
-        return{
-           users : [],
-           isLoading : false,
-           getUsers(){
-            this.isLoading = true;
-             axios.get("https:jsonplaceholder.typicode.com/users").then((res) => {
-                this.users = res.data
-                this.isLoading = false
-             })
-            }
-
+    Alpine.data('usersData', ()=>({
+            users : [],
+            isLoading : false,
+            
+            getUsers(){
+             this.isLoading = true;
+              axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+                 this.users = res.data
+              }).finally(()=>{
+                 this.isLoading = false
+            })
         }
-
-    })
+    }))
 })
