e7d0cb3d4d34e1ba56e70a0f3cd4d0b607919ff5
maedeh-8319 <maedehshobeiri83@gmail.com>
2023-06-03 13:09:20 +0330

    part #24

--- front/js/alpinejscontollers/userscontollores.js
+++ front/js/alpinejscontollers/userscontollores.js
@@ -1,15 +1,28 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', ()=>({
             users : [],
+            pageUsers : [],
             isLoading : false,
             showAddModal : false,
+            pagecount: 1,
+            itemscount: 4,
+            currentpage: 3,
             getUsers(){
              this.isLoading = true;
               axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
                  this.users = res.data
+                 this.pagination()
               }).finally(()=>{
                  this.isLoading = false
             })
+        },
+        pagination(){
+            this.pagecount = Math.ceil(this.users.length / this.itemscount) //10/4=3
+            let start = (this.currentpage * this.itemscount) - this.itemscount  //0
+            let end = this.currentpage * this.itemscount //4
+            this.pageUsers =this.users.slice(start , end) 
+            console.log(this.pageUsers);
         }
+
     }))
 })
